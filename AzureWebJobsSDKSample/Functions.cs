e1c4ed95cfb26c4362bc9c6b9c08fc7d6e4d84cb
TCS <tarak.chandra.sarkar@hotmail.com>
2018-08-21 12:54:50 +0530

    Initial commit

---
+++ AzureWebJobsSDKSample/Functions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AzureWebJobsSDKSample
+{
+    public class Functions
+    {
+        public static void ProcessQueueMessage([QueueTrigger("queue")]string message,
+            [Blob("container/{queueTrigger}", FileAccess.Read)] Stream myBlob,
+            [Blob("container/copy-{queueTrigger}", FileAccess.Write)] Stream outputBlob,
+            ILogger logger)
+        {
+            logger.LogInformation($"Blob name:{message} \n Size: {myBlob.Length} bytes");
+        }
+    }
+}
---
+++ AzureWebJobsSDKSample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace AzureWebJobsSDKSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new JobHostConfiguration();
+            config.DashboardConnectionString = "";
+
+            var loggerFactory = new LoggerFactory();
+            config.LoggerFactory = loggerFactory.AddConsole();
+
+            var host = new JobHost(config);
+            host.RunAndBlock();
+        }
+    }
+}
